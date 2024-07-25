37f00e40de0b16d5ae8f1e67df2074708e29e10d
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2024-07-25 13:42:16 +1000

    Didn't add all the files before...

---
+++ lab2/lecture-node-basics/mods.js
@@ -0,0 +1,11 @@
+module.exports = {
+    add : function (i, j) {
+        return i + j;
+    },
+    sub : function (i, j) {
+        return i - j;
+    },
+    multiply : function (i, j) {
+        return i * j;
+    }
+};
